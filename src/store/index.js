d9856c986c04295e9e2ca9d2d567bb0b59d6399b
wqhash <wqhash@163.com>
2020-06-18 19:29:38 +0800

    require.context()理解

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,17 @@
  * @Description: 
  * @Author: wangqi
  * @Date: 2020-06-14 11:01:31
- * @LastEditTime: 2020-06-17 17:40:00
+ * @LastEditTime: 2020-06-18 19:16:50
  */
 import Vue from 'vue'
 import Vuex from 'vuex'
-import getters from './getters';
+import getters from './getters'
 
 
 Vue.use(Vuex);
 
 const modulesFiles = require.context('./modules', true, /\.js$/);
+// console.log(modulesFiles.resolve("./app.js"),"resolve");
 const modules = modulesFiles.keys().reduce((modules, modulePath) => {
   // set './app.js' => 'app'
   const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1');
