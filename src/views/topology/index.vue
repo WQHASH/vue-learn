19b2a1eaacca66e1ee1e8523be60a92da75aa0af
wqhash <wqhash@163.com>
2020-06-28 21:03:01 +0800

    增加gojs

--- src/views/topology/index.vue
+++ src/views/topology/index.vue
@@ -2,10 +2,48 @@
  * @Description: 
  * @Author: wangqi
  * @Date: 2020-06-28 19:25:58
- * @LastEditTime: 2020-06-28 19:56:01
+ * @LastEditTime: 2020-06-28 20:51:01
 --> 
 <template>
-  <div>
-    <h3>网络托品牌</h3>
-  </div>
+<div>
+    <div id="myDiagramDiv" style="width:600px; height:350px; background-color: #DAE4E4;"></div>
+</div>
 </template>
+
+<script>
+import gojs from "gojs";
+
+export default {
+    data() {
+        return {};
+    },
+    created() {
+        console.log(gojs);
+    },
+    methods: {
+        init() {
+
+            var $ = go.GraphObject.make;
+            var myDiagram =
+                $(go.Diagram, "myDiagramDiv", { // enable Ctrl-Z to undo and Ctrl-Y to redo
+                    "undoManager.isEnabled": true
+                });
+
+            var myModel = $(go.Model);
+            // for each object in this Array, the Diagram creates a Node to represent it
+            myModel.nodeDataArray = [{
+                    key: "Alpha"
+                },
+                {
+                    key: "Beta"
+                },
+                {
+                    key: "Gamma"
+                }
+            ];
+            myDiagram.model = myModel;
+
+        }
+    }
+};
+</script>
