6e1721929e937da1eb16eb6c57464131505b58af
wqhash <wqhash@163.com>
2020-08-05 18:06:51 +0800

    vue官网学习

---
+++ src/views/userManager/components/testCom.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Description: 
+ * @Author: wangqi
+ * @Date: 2020-08-05 15:29:29
+ * @LastEditTime: 2020-08-05 15:35:25
+-->
+><template>
+<div>
+    <input type="text" v-model="num"> -- {{num}}
+</div>
+</template>
+
+<script>
+export default {
+    name: "testCom",
+    data() {
+        return {
+            num: 0,
+        };
+    },
+    mounted() {},
+    methods: {},
+};
+</script>
--- src/views/userManager/menu1-1/index.vue
+++ src/views/userManager/menu1-1/index.vue
@@ -1,5 +1,55 @@
 <template>
- <div style="padding:30px;">
+<div style="padding:30px;">
     <el-alert :closable="false" title="menu 1-1" />
-  </div>
+    <input type="text" placeholder="请输入" @change="changeMethod" v-model.lazy="num"> -- {{num}}
+
+    <input type="checkbox" id="checkbox1" value="1" v-model="checked">
+    <input type="checkbox" id="checkbox2" value="2" v-model="checked">
+    <input type="checkbox" id="checkbox3" value="3" v-model="checked">
+
+    <label for="checkbox">{{ checked }}</label>
+
+    <div id="example-5">
+        <select v-model="selected">
+            <option disabled value="">请选择</option>
+            <option value="aa" checked>A</option>
+            <option value="bb">B</option>
+            <option value="cc">C</option>
+        </select>
+        <span>Selected: {{ selected }}</span>
+    </div>
+
+    <h4>test 组件部分：</h4>
+    <test></test>
+    <test></test>
+    <component :is="current"></component>
+</div>
 </template>
+
+<script>
+import test from '../components/testCom';
+
+export default {
+    data() {
+        return {
+            num: 0,
+            checked: [],
+            selected: "",
+            current:"test",
+            obj: {},
+        };
+    },
+    components:{
+      test
+    },
+    mounted() {},
+    methods: {
+        changeMethod() {
+            console.log(this.num,"num")
+        },
+        keyDown(...args) {
+            console.log(args);
+        },
+    },
+};
+</script>
