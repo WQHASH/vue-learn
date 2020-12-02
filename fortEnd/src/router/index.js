d96f83840eae6fda260cf4118751034f3d7f4bff
wqhash <wqhash@163.com>
2020-12-02 23:25:54 +0800

    base, publicPath理解

--- fortEnd/src/router/index.js
+++ fortEnd/src/router/index.js
@@ -2,7 +2,7 @@
  * @Description: 路由配置
  * @Author: wangqi
  * @Date: 2020-05-29 17:18:31
- * @LastEditTime: 2020-12-02 13:39:39
+ * @LastEditTime: 2020-12-02 23:25:25
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
@@ -23,7 +23,7 @@ routerMethods.forEach(method => {
 const createRouter = () => new VueRouter({
   // history || hash
   mode: "history",
-  // 指定url地址栏中的二级目录
+  // 指定url地址栏应用的基路径(二级路径); 如：127.0.0.1/app/xxx/jjj;  [这里app就是二级,每个单页面都携带]
   base: "/",
   // 检查点击浏览器“前进/后退”按钮时，页面滚动条记录上一次的位置
   scrollBehavior: (to, from, savePosition) => {
